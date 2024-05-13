69395d2232ce6ace7e53c0c2fa80c80bae9499d1
lisz114 <vitoriaelisa114@gmail.com>
2024-05-13 13:29:53 -0300

    DELETE FROM

--- deletes.sql
+++ deletes.sql
@@ -1,1 +1,7 @@
-Delete from Endereco where IdEndereco = 3;
+Delete from Endereco where IdEndereco = ?;
+DELETE FROM produtos WHERE id_produto = ?;
+DELETE FROM Usuarios WHERE idUsuarios = ?; 
+DELETE FROM Produtores WHERE Produtores_idProdutores = ?;
+DELETE FROM vendas WHERE idVenda = ?;
+DELETE FROM Clientes WHERE idClientes = ?;
+DELETE FROM Carrinho WHERE idCarrinho = ?;
