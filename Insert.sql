bceb09822ce28d9a388146061498815d2d1fcffb
lisz114 <vitoriaelisa114@gmail.com>
2024-05-13 12:59:00 -0300

    Insert produtos e usuario

--- Insert.sql
+++ Insert.sql
@@ -1,4 +1,7 @@
-INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (?, ?, ?, ?, ?);
+INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (4, '2029-05-07', 'Asquálio', 49.90, 12);
+INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (5, '2028-20-05', 'Brema', 68.00, 23);
+INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (2, '2032-08-03', 'Lúcio', 69.99, 27);
+
 
 INSERT INTO Produtores(nomeNegocio, Nome, Usuarios_idUsuarios, Enderecos_idEnderecos) VALUES (?, ?, ?, ?);
 
@@ -16,9 +19,9 @@ INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VAL
 
 INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(?, ?);
 
-INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUE(156, 32);
-INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUE(78, 37);
-INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUE(98, 48);
+INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(156, 32);
+INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(78, 37);
+INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(98, 48);
  
 INSERT INTO Clientes (nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES(?, ?, ?, ?);
 
@@ -26,6 +29,8 @@ INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('m
 INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('roger', 43, 69, 41, 4321);
 INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('marcos', 45, 17, 25, 5678);
 
-insert into Usuarios(idUsuarios, nome_Usario, senha_Usuario,email_Usuario) values (?, ?, ?, ?);
+INSERT INTO Usuarios(idUsuarios, nome_Usario, senha_Usuario,email_Usuario) VALUES (6, 'ViniciusGamer', '456123789', 'vinicius.pn13@gmail.com');
+INSERT INTO Usuarios(idUsuarios, nome_Usario, senha_Usuario,email_Usuario) VALUES (4, 'neekolynx114', 'senhafortissima444', 'neeskolyxn114@gmail.com');
+INSERT INTO Usuarios(idUsuarios, nome_Usario, senha_Usuario,email_Usuario) VALUES (8, 'lezikxc', 'senhauau1795', 'lezio456@gmail.com');
 
-insert into enderecos(idEnderecos, Cidade, Rua, Bairro) values (?, ?, ?, ?);
+INSERT INTO enderecos(idEnderecos, Cidade, Rua, Bairro) VALUES (?, ?, ?, ?);
