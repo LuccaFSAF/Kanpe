13c12dc3466c5e54fb18a518a7e2559f7178cc93
Vinicius Peter Nunes <vinicius.pn@aluno.ifsc.edu.br>
2024-05-13 14:11:56 -0300

    Inserts arrumados e criacao do selects all

--- inserts.sql
+++ inserts.sql
@@ -1,40 +1,42 @@
-INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (4, '2029-05-07', 'Asquálio', 49.90, 12);
-INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (5, '2028-20-05', 'Brema', 68.00, 23);
-INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (2, '2032-08-03', 'Lúcio', 69.99, 27);
-
-
 INSERT INTO Produtores(nomeNegocio, Nome, Usuarios_idUsuarios, Enderecos_idEnderecos) VALUES (?, ?, ?, ?);
-
-INSERT INTO Produtores(nomeNegocio, Nome, Usuarios_idUsuarios, Enderecos_idEnderecos) VALUES 
-('Joao', 'Jao Pedro Gonsalves', 4, 2), 
-('Kanepe', 'Pedro Argentino', 5, 10), 
-('Peixes Sao Luiz', 'Matheus Dos Santos Williams', 67, 44);
-
-
 INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VALUES (?, ?, ?);
-
-INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VALUES (27, 123, 456);
-INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VALUES (13, 789, 132);
-INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VALUES (64, 345, 865);
-
 INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(?, ?);
-
-INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(156, 32);
-INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(78, 37);
-INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(98, 48);
- 
 INSERT INTO Clientes (nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES(?, ?, ?, ?);
-
-INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('marcos', 34, 96, 14, 1234);
-INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('roger', 43, 69, 41, 4321);
-INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('marcos', 45, 17, 25, 5678);
-
-INSERT INTO Usuarios(idUsuarios, nome_Usario, senha_Usuario,email_Usuario) VALUES (6, 'ViniciusGamer', '456123789', 'vinicius.pn13@gmail.com');
-INSERT INTO Usuarios(idUsuarios, nome_Usario, senha_Usuario,email_Usuario) VALUES (4, 'neekolynx114', 'senhafortissima444', 'neeskolyxn114@gmail.com');
-INSERT INTO Usuarios(idUsuarios, nome_Usario, senha_Usuario,email_Usuario) VALUES (8, 'lezikxc', 'senhauau1795', 'lezio456@gmail.com');
-
 INSERT INTO enderecos(idEnderecos, Cidade, Rua, Bairro) VALUES (?, ?, ?, ?);
 
-INSERT INTO Endereco(idEnderecos, Cidade, Rua, Bairro) VALUES (1, 'Gaspar', 'Rua da Lombada', 'Centro');
-Insert INTO Endereco(idEnderecos, Cidade, Rua, Bairro) VALUES (2, 'Gaspar', 'Rua de Gaspar', 'Centro');
-Insert INTO Endereco(idEnderecos, Cidade, Rua, Bairro) VALUES (3, 'Blumenau', 'Rua das Flores', 'Velha');
+
+INSERT INTO Enderecos(Cidade, Rua, Bairro) VALUES ('Gaspar', 'Rua da Lombada', 'Centro');
+Insert INTO Enderecos(Cidade, Rua, Bairro) VALUES ('Gaspar', 'Rua de Gaspar', 'Centro');
+Insert INTO Enderecos(Cidade, Rua, Bairro) VALUES ('Blumenau', 'Rua das Flores', 'Velha');
+
+INSERT INTO Usuarios(nome_Usuario, senha_Usuario, email_Usuario) VALUES ('ViniciusGamer', '456123789', 'vinicius.pn13@gmail.com');
+INSERT INTO Usuarios(nome_Usuario, senha_Usuario, email_Usuario) VALUES ('neekolynx114', 'senhafortissima444', 'neeskolyxn114@gmail.com');
+INSERT INTO Usuarios(nome_Usuario, senha_Usuario, email_Usuario) VALUES ('lezikxc', 'senhauau1795', 'lezio456@gmail.com');
+
+INSERT INTO Clientes(nome_cliente, Usuarios_idUsuarios, Enderecos_idEnderecos, nr_casa) VALUES('marcos', 1, 3, 1234);
+INSERT INTO Clientes(nome_cliente, Usuarios_idUsuarios, Enderecos_idEnderecos, nr_casa) VALUES('roger', 2, 2, 4321);
+INSERT INTO Clientes(nome_cliente, Usuarios_idUsuarios, Enderecos_idEnderecos, nr_casa) VALUES('marcos', 3, 1, 5678);
+
+INSERT INTO Produtores(nomeNegocio, Nome, Usuarios_idUsuarios, Enderecos_idEnderecos) VALUES 
+('Joao', 'Jao Pedro Gonsalves', 3, 1), 
+('Kanepe', 'Pedro Argentino', 2, 2), 
+('Peixes Sao Luiz', 'Matheus Dos Santos Williams', 1, 3);
+
+INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (4, '2029-05-07', 'Asquálio', 49.90, 1);
+INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (5, '2028-09-20', 'Brema', 68.00, 2);
+INSERT INTO Produtos(quantidade, validade, nome_Produto, preco, Produtores_idProdutores) VALUES (2, '2032-08-03', 'Lúcio', 69.99, 3);
+
+
+INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VALUES (27, 1, 3);
+INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VALUES (13, 2, 2);
+INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VALUES (64, 3, 1);
+
+INSERT INTO Carrinho(Vendas_IdVendas, Produtos_idProdutos) VALUES(1, 3);
+INSERT INTO Carrinho(Vendas_IdVendas, Produtos_idProdutos) VALUES(2, 2);
+INSERT INTO Carrinho(Vendas_IdVendas, Produtos_idProdutos) VALUES(3, 1);
+
+
+
+
+
+
--- selects.sql
+++ selects.sql
@@ -1,1 +1,7 @@
 Select * from Endereco;
+Select * from Produtos;
+Select * from Produtores;
+Select * from Vendas;
+Select * from Usuarios;
+Select * from Clientes;
+Select * from Carrinho;
