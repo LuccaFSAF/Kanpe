f40e59c04d2f74cd1ba96df95cbb08bdfbd38c7b
Benjamim <benjamim.sb03@aluno.ifsc.edu.br>
2024-05-13 08:45:05 -0300

    insert tabela carrinho e clientes com valores
    ff

--- Insert.sql
+++ Insert.sql
@@ -9,8 +9,16 @@ INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VAL
 INSERT INTO vendas(quantidade, produtores_idProdutores, clientes_idClientes) VALUES (64, 345, 865);
 
 INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUES(?, ?);
+
+INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUE(156, 32);
+INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUE(78, 37);
+INSERT INTO Carrinho(Vendas_Id, Produtos_id) VALUE(98, 48);
  
 INSERT INTO Clientes (nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES(?, ?, ?, ?);
+
+INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('marcos', 34, 96, 14, 1234);
+INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('roger', 43, 69, 41, 4321);
+INSERT INTO Clientes(nome_cliente, Usuarios_id, Enderecos_id, nr_casa) VALUES('marcos', 45, 17, 25, 5678);
 
 insert into Usuarios(idUsuarios, nome_Usario, senha_Usuario,email_Usuario) values (?, ?, ?, ?);
 
